3c52d6fbaaf74df836da8f8ae912a32275182a08
iaquino <iagodeaquino10@gmail.com>
2022-10-07 15:02:56 -0300

    editar e deletar completos

--- index.go
+++ index.go
@@ -47,9 +47,90 @@ func ShowAll(w http.ResponseWriter, r *http.Request) {
 func Greet(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "greet", "olá")
 }
+func Delete(w http.ResponseWriter, r *http.Request) {
+	var id = r.URL.Query().Get("id")
+	bd := conect.Conect()
+	dados, err := bd.Query("SELECT * FROM aluno WHERE id =" + id)
+	if err != nil {
+		log.Println("valor não encontrado")
+	} else {
+		var nome, curso string
+		var idade, matricula, idshow int
+		aluno := model.Aluno{}
+		alunos := []model.Aluno{}
+		for dados.Next() {
+			dados.Scan(&nome, &idade, &matricula, &curso, &idshow)
+			aluno.Nome = nome
+			aluno.Curso = curso
+			aluno.Matricula = matricula
+			aluno.Idade = idade
+			aluno.Id = idshow
+			alunos = append(alunos, aluno)
+		}
+		//comando, err := bd.Prepare("DELETE FROM aluno WHERE id = ?")
+		if err != nil {
+			log.Println("erro ao deletar")
+		} else {
+			//comando.Exec(id)
+			log.Println("deletado com sucesso")
+		}
+		tmpl.ExecuteTemplate(w, "delete", alunos)
+	}
+	defer bd.Close()
+}
+func Edit(w http.ResponseWriter, r *http.Request) {
+	bd := conect.Conect()
+	aluno := model.Aluno{}
+	alunos := []model.Aluno{}
+	var idade, matricula, id int
+	var nome, curso string
+	dados, err := bd.Query("SELECT * FROM aluno WHERE id =?", r.FormValue("id"))
+	if err != nil {
+		log.Println("não encontrado")
+	}
+	for dados.Next() {
+		dados.Scan(&nome, &idade, &matricula, &curso, &id)
+		aluno.Nome = nome
+		aluno.Curso = curso
+		aluno.Matricula = matricula
+		aluno.Idade = idade
+		aluno.Id = id
+		alunos = append(alunos, aluno)
+	}
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		idade := r.FormValue("idade")
+		matricula := r.FormValue("matricula")
+		curso := r.FormValue("curso")
+		idE := r.FormValue("id")
+		comando, err := bd.Prepare("UPDATE aluno SET nome=? , idade=? , matricula=? , curso=? WHERE id=?")
+		if err != nil {
+			log.Println("error com os dados")
+		} else {
+			comando.Exec(nome, idade, matricula, curso, idE)
+		}
+	}
+	dados, err = bd.Query("SELECT * FROM aluno WHERE id =?", r.FormValue("id"))
+	if err != nil {
+		log.Println("não encontrado")
+	}
+	for dados.Next() {
+		dados.Scan(&nome, &idade, &matricula, &curso, &id)
+		aluno.Nome = nome
+		aluno.Curso = curso
+		aluno.Matricula = matricula
+		aluno.Idade = idade
+		aluno.Id = id
+		alunos = append(alunos, aluno)
+	}
+	tmpl.ExecuteTemplate(w, "edit", alunos)
+	defer bd.Close()
+}
 func main() {
 	log.Println("inicializando servidor na porta:9090")
 	http.HandleFunc("/show", ShowAll)
+	http.HandleFunc("/delete", Delete)
+	http.HandleFunc("/edit", Edit)
 	http.HandleFunc("/", Greet)
 	http.ListenAndServe(":9090", nil)
 }
